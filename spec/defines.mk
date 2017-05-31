203032583c92d0537d50fc715915c7c5b89b53f9
Hanno Kolvenbach <kolvenbach@silexica.com>
2017-05-31 13:12:59 +0200

    Added default platform

--- spec/defines.mk
+++ spec/defines.mk
@@ -2,3 +2,4 @@ TARGET             := usermake
 BUILDTYPE          := userproject
 USER_BUILD_AND_RUN := $(MAKE) -C ../myapp -f mymakefile.mk helloapp run
 USER_CLEAN         := $(MAKE) -C ../myapp -f mymakefile.mk clean
+PLATFORM           :=/home/user/silexica/platforms/platform_descriptions/exynos.platform
